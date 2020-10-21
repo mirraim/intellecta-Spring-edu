b9617da15af45c7eb8274ec98615ea24d3405bff
Maria Pankratova <mirraim@gmail.com>
2020-10-21 21:29:47 +0500

    Spring Boot course lesson1

--- src/main/java/ru/education/controllers/TestController.java
+++ src/main/java/ru/education/controllers/TestController.java
@@ -5,17 +5,24 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.education.jpa.Product;
+import ru.education.jpa.ProductRepository;
 import ru.education.model.Formatter;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
 public class TestController {
 
+    private final ProductRepository productRepository;
+
     private final Formatter formatter;
 
     @Autowired
-    public TestController(@Qualifier("fooFormatter") Formatter formatter) {
+    public TestController(@Qualifier("fooFormatter") Formatter formatter, ProductRepository productRepository) {
         this.formatter = formatter;
+        this.productRepository = productRepository;
     }
 
     @GetMapping("/hello")
@@ -26,5 +33,10 @@ public class TestController {
     @GetMapping("/format")
     public String getFormat(){
         return formatter.format();
+    }
+
+    @GetMapping("/products")
+    public List<Product> getProduct(){
+        return productRepository.findAll();
     }
 }
---
+++ src/main/java/ru/education/jpa/Product.java
@@ -0,0 +1,26 @@
+package ru.education.jpa;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+}
